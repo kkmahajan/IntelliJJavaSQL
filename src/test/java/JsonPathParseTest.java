ce7b0988872d12e574c9cb6ac88a5bc0be4690ff
Kaustubh <kstbhmahajan@gmail.com>
2024-05-24 22:51:04 +0530

    Optimize space

--- src/test/java/JsonPathParseTest.java
+++ src/test/java/JsonPathParseTest.java
@@ -13,51 +13,51 @@ public class JsonPathParseTest {
 
     // Print no of courses returned by API
     @Test
-    public void printNoOfCourses(){
+    public void printNoOfCourses() {
         // .size() can be applied only to array []
-        System.out.println("No of Courses in the Response : "+jsonPath.getInt("courses.size()"));
+        System.out.println("No of Courses in the Response : " + jsonPath.getInt("courses.size()"));
     }
 
     @Test
-    public void printPurchaseAmountAsInt(){
-        System.out.println("The Purchase amount is : "+jsonPath.getInt("dashboard.purchaseAmount"));
+    public void printPurchaseAmountAsInt() {
+        System.out.println("The Purchase amount is : " + jsonPath.getInt("dashboard.purchaseAmount"));
     }
 
     @Test
-    public void printPurchaseAmountAsString(){
-        System.out.println("The Purchase amount is : "+jsonPath.getString("dashboard.purchaseAmount"));
+    public void printPurchaseAmountAsString() {
+        System.out.println("The Purchase amount is : " + jsonPath.getString("dashboard.purchaseAmount"));
     }
 
     @Test
-    public void getFirstCourseTitle(){
-        System.out.println("The first course title is : "+jsonPath.getString("courses[0].title"));
+    public void getFirstCourseTitle() {
+        System.out.println("The first course title is : " + jsonPath.getString("courses[0].title"));
     }
 
     @Test
-    public void printAllCoursesAndTheirPrice(){
-        for(int i = 0; i<jsonPath.getInt("courses.size()"); i++){
-            System.out.println("Course "+(i+1)+" :"+jsonPath.getString("courses.title["+i+"]"));
-            System.out.println("Price of Course "+(i+1)+" :"+jsonPath.getString("courses.price["+i+"]"));
+    public void printAllCoursesAndTheirPrice() {
+        for (int i = 0; i < jsonPath.getInt("courses.size()"); i++) {
+            System.out.println("Course " + (i + 1) + " :" + jsonPath.getString("courses.title[" + i + "]"));
+            System.out.println("Price of Course " + (i + 1) + " :" + jsonPath.getString("courses.price[" + i + "]"));
         }
     }
 
     @Test
-    public void printCopiesSoldByRPACourse(){
-        for(int i = 0; i<jsonPath.getInt("courses.size()"); i++){
-            String courseTitles = jsonPath.get("courses.title["+i+"]");
-            if (courseTitles.equalsIgnoreCase("RPA")){
-                System.out.println("The number of copies sold for RPA : "+jsonPath.getString("courses.copies["+i+"]"));
+    public void printCopiesSoldByRPACourse() {
+        for (int i = 0; i < jsonPath.getInt("courses.size()"); i++) {
+            String courseTitles = jsonPath.get("courses.title[" + i + "]");
+            if (courseTitles.equalsIgnoreCase("RPA")) {
+                System.out.println("The number of copies sold for RPA : " + jsonPath.getString("courses.copies[" + i + "]"));
                 break;
             }
         }
     }
 
     @Test
-    public void verifyAllThePricesAddUpToTheTotalAmount(){
+    public void verifyAllThePricesAddUpToTheTotalAmount() {
         int priceAmount = 0;
-        for(int i = 0; i<jsonPath.getInt("courses.size()"); i++){
-            int copies = jsonPath.getInt("courses.copies["+i+"]");
-            int price = jsonPath.getInt("courses.price["+i+"]");
+        for (int i = 0; i < jsonPath.getInt("courses.size()"); i++) {
+            int copies = jsonPath.getInt("courses.copies[" + i + "]");
+            int price = jsonPath.getInt("courses.price[" + i + "]");
             priceAmount = priceAmount + (copies * price);
         }
         int totalPriceAmountInResponse = jsonPath.getInt("dashboard.purchaseAmount");
--- src/test/java/RestAssuredTestOne.java
+++ src/test/java/RestAssuredTestOne.java
@@ -2,17 +2,16 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import io.restassured.RestAssured;
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.equalTo;
-
 import io.restassured.path.json.JsonPath;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Random;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class RestAssuredTestOne {
 
@@ -27,40 +26,29 @@ public class RestAssuredTestOne {
     @Test
     public void TestOne() {
         RestAssured.baseURI = "https://rahulshettyacademy.com/maps/api/place/";
-        String addResponse = given().log().all()
-                .queryParam("key",key).header("Content-Type","application/json")
-                .body(createRequestBodyForAddPlace())
-        .when().post("add/json")
-        .then().assertThat().statusCode(200).body("scope", equalTo("APP")).extract().response().asString();
+        String addResponse = given().log().all().queryParam("key", key).header("Content-Type", "application/json").body(createRequestBodyForAddPlace()).when().post("add/json").then().assertThat().statusCode(200).body("scope", equalTo("APP")).extract().response().asString();
 
         JsonPath jsonPath = new JsonPath(addResponse);
         place_id = jsonPath.getString("place_id");
-        System.out.println("Place Id : "+place_id);
+        System.out.println("Place Id : " + place_id);
 
         // Update Place
-        given().log().all()
-        .queryParam("key","qaclick123").header("Content-Type","application/json")
-        .body(createRequestBodyForUpdatePlace())
-        .when().put("update/json")
-        .then().assertThat().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
+        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json").body(createRequestBodyForUpdatePlace()).when().put("update/json").then().assertThat().log().all().statusCode(200).body("msg", equalTo("Address successfully updated"));
 
 
         // Get Place
-        String getPlaceResponse = given().log().all()
-                .queryParam("key",key).queryParam("place_id", place_id)
-                .when().get("get/json")
-                .then().assertThat().log().all().statusCode(200).extract().response().asString();
+        String getPlaceResponse = given().log().all().queryParam("key", key).queryParam("place_id", place_id).when().get("get/json").then().assertThat().log().all().statusCode(200).extract().response().asString();
 
         JsonPath jsonPath1 = new JsonPath(getPlaceResponse);
         String actualAddress = jsonPath1.getString("address");
-        System.out.println("The Actual Address in Get Place after Update Place Response is : "+actualAddress);
+        System.out.println("The Actual Address in Get Place after Update Place Response is : " + actualAddress);
 
 //        ExtentTestManager.getExtentTest().log(Status.INFO, "Additional information...");
 //        ExtentTestManager.getExtentTest().pass("Test passed!");
 //        ExtentManager.flush(); // Flush ExtentReports
     }
 
-    public Object createRequestBodyForAddPlace(){
+    public Object createRequestBodyForAddPlace() {
         HashMap<String, Object> requestBodyAddPlace = new HashMap<>();
         HashMap<String, Object> location = new HashMap<>();
         ArrayList<String> typesData = new ArrayList<>();
@@ -72,14 +60,14 @@ public class RestAssuredTestOne {
         double longitude = random.nextDouble() * 360 - 180; // Generate a random value between -180 and 180
         location.put("lat", latitude);
         location.put("lng", longitude);
-        String placeNumber = String.valueOf(1+random.nextInt(99));
+        String placeNumber = String.valueOf(1 + random.nextInt(99));
 
         requestBodyAddPlace.put("location", location);
         requestBodyAddPlace.put("types", typesData);
         requestBodyAddPlace.put("accuracy", 50);
-        requestBodyAddPlace.put("name", placeNumber+", Frontline House");
+        requestBodyAddPlace.put("name", placeNumber + ", Frontline House");
         requestBodyAddPlace.put("phone_number", "(+91)0987654321");
-        requestBodyAddPlace.put("address", random.nextInt(100) +", side layout, cohen 09");
+        requestBodyAddPlace.put("address", random.nextInt(100) + ", side layout, cohen 09");
         requestBodyAddPlace.put("website", "http://google.com");
         requestBodyAddPlace.put("language", "English-EN");
 
@@ -92,10 +80,11 @@ public class RestAssuredTestOne {
 
         requestBodyUpdatePlace.put("place_id", place_id);
         requestBodyUpdatePlace.put("address", "BackLine House");
-        requestBodyUpdatePlace.put("key",key);
+        requestBodyUpdatePlace.put("key", key);
 
         return requestBodyUpdatePlace;
     }
+
     @Test
     public void printPrettyJson() throws JsonProcessingException {
 
@@ -103,16 +92,16 @@ public class RestAssuredTestOne {
 
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-        System.out.println("Following is the JSON Format of the HashMap :\n"+objectMapper.writeValueAsString(obj));
+        System.out.println("Following is the JSON Format of the HashMap :\n" + objectMapper.writeValueAsString(obj));
     }
 
     @DataProvider(name = "LatLongData")
-    public Object[][] latLongData(){
-        return new Object[][] {{-1.086320854785452, -124.684349682051},{-6.086320854745452, -124.984149684051},{-1.086322354745452, -124.6834684051}};
+    public Object[][] latLongData() {
+        return new Object[][]{{-1.086320854785452, -124.684349682051}, {-6.086320854745452, -124.984149684051}, {-1.086322354745452, -124.6834684051}};
     }
 
     @Test(dataProvider = "LatLongData")
-    public void addPlaceWithDataProvider(double latitude, double longitude){
+    public void addPlaceWithDataProvider(double latitude, double longitude) {
 
         HashMap<String, Object> requestBodyAddPlace = new HashMap<>();
         HashMap<String, Object> location = new HashMap<>();
@@ -123,24 +112,20 @@ public class RestAssuredTestOne {
 
         location.put("lat", latitude);
         location.put("lng", longitude);
-        String placeNumber = String.valueOf(1+random.nextInt(99));
+        String placeNumber = String.valueOf(1 + random.nextInt(99));
 
         requestBodyAddPlace.put("location", location);
         requestBodyAddPlace.put("types", typesData);
         requestBodyAddPlace.put("accuracy", 50);
-        requestBodyAddPlace.put("name", placeNumber+", Frontline House");
+        requestBodyAddPlace.put("name", placeNumber + ", Frontline House");
         requestBodyAddPlace.put("phone_number", "(+91)0987654321");
-        requestBodyAddPlace.put("address", random.nextInt(100) +", side layout, cohen 09");
+        requestBodyAddPlace.put("address", random.nextInt(100) + ", side layout, cohen 09");
         requestBodyAddPlace.put("website", "http://google.com");
         requestBodyAddPlace.put("language", "English-EN");
 
 
         RestAssured.baseURI = "https://rahulshettyacademy.com/maps/api/place/";
-        String addResponseDataProvider = given().log().all()
-                .queryParam("key",key).header("Content-Type","application/json")
-                .body(requestBodyAddPlace)
-                .when().post("add/json")
-                .then().assertThat().statusCode(200).body("scope", equalTo("APP")).extract().response().asString();
+        String addResponseDataProvider = given().log().all().queryParam("key", key).header("Content-Type", "application/json").body(requestBodyAddPlace).when().post("add/json").then().assertThat().statusCode(200).body("scope", equalTo("APP")).extract().response().asString();
 
         JsonPath jsonPath = new JsonPath(addResponseDataProvider);
         System.out.println(jsonPath);
