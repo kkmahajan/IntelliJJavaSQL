a05056014dc8d546406134de1995ed0bf9214f14
Kaustubh <kstbhmahajan@gmail.com>
2024-05-24 23:42:30 +0530

    Update with Streams examples

---
+++ src/test/java/JavaFeaturesTest.java
@@ -0,0 +1,74 @@
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+public class JavaFeaturesTest {
+
+    @Test
+    public void testStreams() {
+
+        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
+        // Basic print without Streams
+        System.out.println("Basic Print the list");
+        System.out.println(integerList);
+
+        System.out.println("Print using the conventional for loop");
+        //Print using conventional for loop
+        for (int i = 0; i < integerList.size(); i++) {
+            if (integerList.get(i) % 2 == 0) {
+                System.out.println("The number " + integerList.get(i) + " is even.");
+            }
+        }
+
+        System.out.println("Print using enhanced for loop");
+        //Print using enhanced for loop
+        for (int n : integerList) {
+            if (n % 2 == 0) {
+                System.out.println("The number " + n + " is even.");
+            }
+        }
+
+        System.out.println("Print using foreach without lambda function");
+        //Print using for each without lambda function
+        integerList.forEach(n -> System.out.println(n));
+
+        System.out.println("Print using foreach with lambda function");
+        //Print using for each with Lambda function
+        integerList.forEach(System.out::println);
+
+        //Print using for each with custom lambda function
+        System.out.println("Print numbers + 1 using foreach with customs made lambda function accept with type Integer");
+
+        Consumer<Integer> consumer = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(integer + 1);
+            }
+        };
+        integerList.forEach(consumer);
+
+        //Print using Custom lambda function with other type
+        System.out.println("print using custom made lambda function");
+        Consumer<Integer> consumer1 = System.out::println;
+        Consumer<Integer> consumer2 = n -> System.out.println(n + 1);
+
+        integerList.forEach(consumer1);
+        integerList.forEach(consumer2);
+
+
+        // Print using Streams
+        System.out.println("Print using Streams and above mentioned foreach lambda function to add + 1");
+        Stream<Integer> stream = integerList.stream();
+        stream.forEach(consumer2);
+
+        //Print using Streams and filter method of Streams
+        System.out.println("Print using Streams along with the defined method only even numbers from the list");
+        Stream<Integer> stream2 = integerList.stream();
+        Stream<Integer> stream1 = stream2.filter(n -> n % 2 == 0);
+        Stream<Integer> stream3 = stream1.map(n->n*2);
+        stream3.forEach(consumer1);
+    }
+}
