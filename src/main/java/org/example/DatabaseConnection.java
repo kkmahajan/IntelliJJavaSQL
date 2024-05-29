5110d61090ef882ad0626e07ab6cc01c717945dc
Kaustubh <kstbhmahajan@gmail.com>
2024-05-29 21:02:48 +0530

    Refactor

--- src/main/java/org/example/DatabaseConnection.java
+++ src/main/java/org/example/DatabaseConnection.java
@@ -1,39 +1,55 @@
 package org.example;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class DatabaseConnection {
     public static void main(String[] args) {
-        HikariConfig config = new HikariConfig();
-        config.setJdbcUrl("jdbc:mysql://localhost:3306/dbo");
-        config.setUsername("root");
-        config.setPassword("admin");
 
-        // Set the connection timeout
-        config.setConnectionTimeout(10000); // 10 seconds
+        final Logger LOGGER = Logger.getLogger(DatabaseConnection.class.getName());
+        DriverManager.setLoginTimeout(10);
+        try {
+            String connectionUrl = "jdbc:mysql://localhost:3306/dbo";
+            Connection con = DriverManager.getConnection(connectionUrl, "root", "admin");
+            Statement statement = con.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM EMPLOYEE");
+            List<Map<String, Object>> resultList = new ArrayList<>();
 
-        DataSource dataSource = new HikariDataSource(config);
+            // Get the metadata of the ResultSet to retrieve column names
+            int columnCount = resultSet.getMetaData().getColumnCount();
 
-        try (Connection connection = dataSource.getConnection()) {
-            System.out.println("Connected to the database");
-            // Create a statement and set query timeout
-            try (Statement statement = connection.createStatement()) {
-                statement.setQueryTimeout(5); // 5 seconds
-                ResultSet resultSet = statement.executeQuery("SELECT * FROM EMPLOYEE");
-                while (resultSet.next()) {
-                    System.out.println(resultSet.getString("empname"));
-                    System.out.println(resultSet.getString("empId"));
-                }
+            List<String> columnNames = new ArrayList<>();
+            for (int i = 1; i <= columnCount; i++) {
+                columnNames.add(resultSet.getMetaData().getColumnName(i));
             }
+
+            // Iterate over the ResultSet and populate the list
+            while (resultSet.next()) {
+                Map<String, Object> rowMap = new HashMap<>();
+                for (String columnName : columnNames) {
+                    rowMap.put(columnName, resultSet.getObject(columnName));
+                }
+                resultList.add(rowMap);
+            }
+            System.out.println("First Row : " + resultList.getFirst());
+            System.out.println("First Emp Name : " + resultList.getFirst().get("EmpName"));
+            System.out.println("First Emp Id Class : " + resultList.getFirst().get("EmpId").getClass());
+            System.out.println("First Emp Id : " + resultList.getFirst().get("EmpId"));
+
+            // Following code will print the result of the result set one by one.
+            /*
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("empname"));
+                System.out.println(resultSet.getString("empId"));
+            }
+            */
         } catch (SQLException e) {
-            e.printStackTrace();
+            LOGGER.log(Level.SEVERE, "SQL Exception occurred", e);
         }
     }
 }
